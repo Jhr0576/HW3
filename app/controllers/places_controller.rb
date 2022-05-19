3b482bd8271a3e8860fa80fb42fb2c8dbfdd8e42
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-05-19 03:56:15 +0000

    Save3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,5 @@
 class PlacesController < ApplicationController
 
-    def index
-
-        @place1 = Cass Lake
-        @place2 = Banff
-        @place3 = Grand Canyon
-        @place_new = New Place
-
-    end
+   
 
 end
