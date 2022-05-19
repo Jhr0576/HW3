b988e4eed8ec4e064120ad63b1b41b448e1b600d
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-05-19 21:12:27 +0000

    Save24

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,2 +1,7 @@
 class LogController < ApplicationController
+
+def index
+
+end
+
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,11 @@
 class PlacesController < ApplicationController
 
-   
+   def index 
+
+    @place1 = "Cass Lake"
+    @place2 = "Banff"
+    @place3 = "Grand Canyon"
+
+   end
 
 end
