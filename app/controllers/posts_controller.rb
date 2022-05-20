926227462fa6daf3c9bd6128c87e8c69342c87b4
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-05-20 00:29:28 +0000

    Save43

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,7 @@ class PostsController < ApplicationController
   
     def new
       @post = Post.new
-      @post = Post.find_by({ "id" => params["place_id"] })
-      @post["place_id"] = @place["id"]
+      @post["place_id"] = params["place_id"]
     end
   
     def create
